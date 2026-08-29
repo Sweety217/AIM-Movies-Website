af813eb6e2bb388ee3bfe70146ae706419770c5d
Sweety217 <roydiya217147@gmail.com>
2026-08-29 15:05:48 +0530

    add new updated style

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const navLinks = document.getElementById("navLinks");
 
 hamburgerIcon.addEventListener("click", ()=>{
   navLinks.classList.toggle("openMenu");
+  if (navLinks.classList.contains("openMenu")) {
+    document.body.style.overflow="hidden"
+  } else {
+        document.body.style.overflow="auto"
+  }
 })
  const wrap1 = document.getElementById('wrap1');
   const wrap2 = document.getElementById('wrap2');
